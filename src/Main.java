ad3e18eda881cfe22822e978ba19454872d3626a
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-13 23:26:02 +0300

    Changed Main2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int balance = 100; // баланс
         int addBalance = 300; // пополнение баланса
         int bonus;
